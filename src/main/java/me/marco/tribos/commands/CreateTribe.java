c02b63a0c0fb79e84b67e3e16416cfda108b7e55
Marco Silva <marstrabalhar@gmail.com>
2024-05-25 17:28:53 +0100

    added /tribe command to open tribe menu

--- src/main/java/me/marco/tribos/commands/CreateTribe.java
+++ src/main/java/me/marco/tribos/commands/CreateTribe.java
@@ -1,10 +1,15 @@
 package me.marco.tribos.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Objects;
 
@@ -18,14 +23,15 @@ public class CreateTribe implements CommandExecutor {
         }
         Player player = (Player) commandSender;
 
-        if(params.length == 2 && params[0].equalsIgnoreCase("create")) {
-            if (Objects.equals(params[1], null)) {
-                player.sendMessage("§cYou should specify the name of the tribe!");
-                return true;
-            }
-                player.sendMessage("§aYou have created the tribe: " + ChatColor.GOLD+params[1]);
-                return true;
-        }
-        return false;
+            Inventory inventory = Bukkit.createInventory(player, 27, "§4Tribes menu");
+            ItemStack getHayItem = new ItemStack(Material.HAY_BLOCK);
+            ItemMeta getHayItemMeta = getHayItem.getItemMeta();
+            assert getHayItemMeta != null;
+            getHayItemMeta.setDisplayName("§7Create a new Tribe");
+            getHayItem.setItemMeta(getHayItemMeta);
+            inventory.setItem(11, getHayItem);
+            player.openInventory(inventory);
+
+            return true;
     }
 }
